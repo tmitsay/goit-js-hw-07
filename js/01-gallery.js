2987b9427befb7a2f3c7fc7360dc010d76ec2535
tmitsay <132346077+tmitsay@users.noreply.github.com>
2023-09-07 20:48:27 +0300

    done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,55 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const list = document.querySelector(".gallery");
+
+list.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+
+list.addEventListener("click", onImgElement);
+
+function onImgElement(event) {
+  event.preventDefault();
+
+  if (event.target === event.currentTarget) {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" alt="${event.target.alt}" />`,
+    {
+      onShow: (instance) => {
+        document.addEventListener("keydown", onKeyEsc);
+      },
+      onClose: (instance) => {
+        document.removeEventListener("keydown", onKeyEsc);
+      },
+    }
+  );
+
+  instance.show();
+
+  function onKeyEsc(event) {
+    if (event.code === "Escape") {
+      return instance.close();
+    }
+  }
+  // console.dir(event.target);
+}
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) => `<li class="gallery__item">
+    <a class='gallery__link' href="${original}">
+
+    <img class="gallery__image"
+    src="${preview}" alt="${description}" 
+    data-source="${original}"/>
+
+    </a>
+      </li>`
+    )
+    .join("");
+}
